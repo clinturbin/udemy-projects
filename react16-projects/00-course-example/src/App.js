bd447fdb460f237b4404e9c5a907873864611cff
clinturbin <clinturbin@gmail.com>
2018-10-19 15:24:08 -0400

    Manipulating the state

--- react16-projects/00-course-example/src/App.js
+++ react16-projects/00-course-example/src/App.js
@@ -3,15 +3,38 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: 'Max', age: '28'},
+      {name: 'Michael', age: '29'},
+      {name: 'Isobel', age: '30'},
+    ]
+
+  }
+
+  switchNameHandler = () => {
+    this.setState({
+      persons: [
+        {name: 'Maximus', age: '28'},
+        {name: 'Michael', age: '29'},
+        {name: 'Isobel', age: '31'},
+      ]
+    })
+  }
+
   render() {
-    // return React.createElement('div', {className:'App'}, React.createElement('h1', null,  `Hi I'm a React App`));
     return (
       <div className='App'>
         <h1>Hi, I'm a React app</h1>
         <p>This is really working</p>
-        <Person name='Max' age='28'/>
-        <Person name='Michael' age='29'>My hobbies: Racing</Person>
-        <Person name='Isabel' age='30'/>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person 
+          click={this.switchNameHandler}
+          name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person 
+          name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     )
   }
