7144ff58a5878efa5300482a0145ff47c553eda3
clinturbin <clinturbin@gmail.com>
2018-10-20 11:44:58 -0400

    Adding two way binding and styling

--- react16-projects/00-course-example/src/App.js
+++ react16-projects/00-course-example/src/App.js
@@ -12,27 +12,49 @@ class App extends Component {
 
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     this.setState({
       persons: [
-        {name: 'Maximus', age: '28'},
+        {name: newName, age: '28'},
         {name: 'Michael', age: '29'},
         {name: 'Isobel', age: '31'},
       ]
     })
   }
 
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        {name: 'Max', age: '28'},
+        {name: event.target.value, age: '29'},
+        {name: 'Isobel', age: '30'},
+      ]
+    })
+  }
+
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
     return (
       <div className='App'>
         <h1>Hi, I'm a React app</h1>
         <p>This is really working</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <button 
+          style={style}
+          onClick={() => this.switchNameHandler('Maximus!!')}>Switch Name</button>
         <Person 
           name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person 
-          click={this.switchNameHandler}
-          name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Max!')}
+          changed={this.nameChangedHandler}/>
         <Person 
           name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
--- react16-projects/00-course-example/src/Person/Person.js
+++ react16-projects/00-course-example/src/Person/Person.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './Person.css';
 
 const person = (props) => 
     
-    <div>
+    <div className='Person'>
         <p onClick={props.click}>I'm {props.name} and I'm {props.age} year's old!</p>
         <p>{props.children}</p>
+        <input type='text' onChange={props.changed} value={props.name}/>
     </div>
 
 export default person;
