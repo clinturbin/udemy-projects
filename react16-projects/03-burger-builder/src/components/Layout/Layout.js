cd1605efe56e4d2ccab3c1e234604f8ef5be82d2
clinturbin <clinturbin@gmail.com>
2018-10-26 15:19:25 -0400

    Starting Implementation of the Burger Builder Container and Adding a Dynamic Ingredient Component

--- react16-projects/03-burger-builder/src/components/Layout/Layout.js
+++ react16-projects/03-burger-builder/src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import Aux from '../../hoc/Aux';
+import classes from './Layout.module.css';
 
 const layout = (props) => (
     <Aux>
         <div>Toolbar, SideDrawer, Backdrop</div>
-        <main>
+        <main className={classes.Content}>
             {props.children}
         </main>
     </Aux>
