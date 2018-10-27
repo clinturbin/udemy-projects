12401158791205ff90a02849ac35adb9bec1cf65
clinturbin <clinturbin@gmail.com>
2018-10-27 10:30:55 -0400

    Outputting Burger Ingedients Dynamically

--- react16-projects/03-burger-builder/src/components/Burger/Burger.js
+++ react16-projects/03-burger-builder/src/components/Burger/Burger.js
@@ -4,11 +4,22 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    let transformedIngredients = Object.keys(props.ingredients).map(igKey => {
+        return [...Array(props.ingredients[igKey])].map((_,index) => {
+            return <BurgerIngredient key={igKey + index} type={igKey} />;
+        });
+    }).reduce((arr, el) => {
+        return arr.concat(el)
+    }, []);
+    console.log(transformedIngredients);
+
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding Ingredients</p>
+    }
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="meat"/>
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom"/>
         </div>
     );
