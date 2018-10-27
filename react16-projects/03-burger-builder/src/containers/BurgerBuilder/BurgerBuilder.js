401f6b408d1f01c46223c58f9685f54248dcefd7
clinturbin <clinturbin@gmail.com>
2018-10-27 13:48:19 -0400

    Displaying and Updating the Burger Price and Adding the Order Button

--- react16-projects/03-burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ react16-projects/03-burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -19,7 +19,17 @@ class BurgerBuilder extends Component {
             cheese: 0,
             meat: 0
         },
-        totalPrice: 4
+        totalPrice: 4,
+        purchasable: false
+    };
+
+    updatePurchaseState(ingredients) {
+        const sum = Object.keys(ingredients).map(igKey => {
+            return ingredients[igKey];
+        }).reduce((sum, el) => {
+            return sum + el;
+        }, 0);
+        this.setState({purchasable: sum > 0})
     };
 
     addIngredientHandler = (type) => {
@@ -34,6 +44,7 @@ class BurgerBuilder extends Component {
             ingredients: updatedIngredients,
             totalPrice: newPrice
         });
+        this.updatePurchaseState(updatedIngredients);
     };
     
     removeIngredientHandler = (type) => {
@@ -51,6 +62,7 @@ class BurgerBuilder extends Component {
             ingredients: updatedIngredients,
             totalPrice: newPrice
         });
+        this.updatePurchaseState(updatedIngredients);
     };
 
     render() {
@@ -67,6 +79,8 @@ class BurgerBuilder extends Component {
                     ingredientAdded={this.addIngredientHandler}
                     ingredientRemoved={this.removeIngredientHandler}
                     disabled={disabledInfo}
+                    price={this.state.totalPrice}
+                    purchasable={this.state.purchasable}
                 />
             </Aux>
         );
