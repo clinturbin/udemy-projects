93ba6d9c2b3db8d7832f6ddddd351cb13c2757a4
clinturbin <clinturbin@gmail.com>
2018-10-30 11:24:00 -0400

    Adding Reusable Navigation Items and Creating a Responsive SideDrawer

---
+++ react16-projects/03-burger-builder/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import classes from './SideDrawer.module.css';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+
+const sideDrawer = (props) => {
+    
+    return (
+        <div className={classes.SideDrawer}>
+            <Logo/>
+            <nav>
+                <NavigationItems/>
+            </nav>
+        </div>
+    );
+};
+
+export default sideDrawer;
