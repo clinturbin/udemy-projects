fa18d9ef60ef368ba5e13d77e07dddbe935a193a
clinturbin <clinturbin@gmail.com>
2018-11-05 09:44:45 -0500

    React 16 Section 8 Reusing the Backdrop and Adding SideDrawer Toggle Button and Hamburger Icon

--- react16-projects/03-burger-builder/src/components/Navigation/SideDrawer/SideDrawer.js
+++ react16-projects/03-burger-builder/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,18 +3,26 @@ import React from 'react';
 import classes from './SideDrawer.module.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Aux';
 
 const sideDrawer = (props) => {
-    
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+    if (props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open];
+    }
     return (
-        <div className={classes.SideDrawer}>
-        <div className={classes.Logo}>
-            <Logo/>
-        </div>
-            <nav>
-                <NavigationItems/>
-            </nav>
-        </div>
+        <Aux>
+            <Backdrop show={props.open} clicked={props.closed}/>
+            <div className={attachedClasses.join(' ')}>
+                <div className={classes.Logo}>
+                    <Logo/>
+                </div>
+                <nav>
+                    <NavigationItems/>
+                </nav>
+            </div>
+        </Aux>
     );
 };
 
