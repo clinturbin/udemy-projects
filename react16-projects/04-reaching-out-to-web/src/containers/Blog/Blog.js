6072047969d45ee4aaabd3f125851101a0ce6516
clinturbin <clinturbin@gmail.com>
2018-11-07 10:38:14 -0500

    React 16 Section 9 Creating HTTP Request through Transfroming Data

--- react16-projects/04-reaching-out-to-web/src/containers/Blog/Blog.js
+++ react16-projects/04-reaching-out-to-web/src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,13 +7,41 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+    state = {
+        posts: []
+    }
+
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(response => {
+            const posts = response.data.slice(0, 4); // Only store the first 4 posts
+            const updatedPosts = posts.map(post => {
+                return {
+                    ...post,
+                    author: 'C-Bot'
+                }
+            })
+            // this.setState({posts: response.data});
+            this.setState({posts: updatedPosts});
+            // console.log(response);
+        });
+    }
+
     render () {
+        const posts = this.state.posts.map(post => {
+            return (
+                <Post 
+                    key={post.id}
+                    title={post.title}
+                    author={post.author}
+                />
+            );
+        });
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts};
                 </section>
                 <section>
                     <FullPost />
