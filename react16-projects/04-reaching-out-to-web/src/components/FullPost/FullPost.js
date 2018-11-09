a3fa2e54564b45f095b2c53d48d01258509232f4
clinturbin <clinturbin@gmail.com>
2018-11-09 08:18:31 -0500

    React 16 Section 9 Posting Data to the Server through Handling Errors Locally

--- react16-projects/04-reaching-out-to-web/src/components/FullPost/FullPost.js
+++ react16-projects/04-reaching-out-to-web/src/components/FullPost/FullPost.js
@@ -20,6 +20,15 @@ class FullPost extends Component {
             
         }
     }
+
+    // delete request will not actually delete anything, it will just just simulate deleting a post for now
+    deletePostHandler = () => {
+        axios.delete(`https://jsonplaceholder.typicode.com/posts/${this.props.id}`)
+        .then(response => {
+            console.log(response);
+        });
+    }
+
     render () {
         let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
         if (this.props.id) {
@@ -31,7 +40,7 @@ class FullPost extends Component {
                     <h1>{this.state.loadedPost.title}</h1>
                     <p>{this.state.loadedPost.body}</p>
                     <div className="Edit">
-                        <button className="Delete">Delete</button>
+                        <button onClick={this.deletePostHandler} className="Delete">Delete</button>
                     </div>
                 </div>
             );
--- react16-projects/04-reaching-out-to-web/src/containers/Blog/Blog.js
+++ react16-projects/04-reaching-out-to-web/src/containers/Blog/Blog.js
@@ -9,7 +9,8 @@ import './Blog.css';
 class Blog extends Component {
     state = {
         posts: [],
-        selectedPostId: null
+        selectedPostId: null,
+        error: false
     }
 
     componentDidMount() {
@@ -23,6 +24,11 @@ class Blog extends Component {
                 }
             })
             this.setState({posts: updatedPosts});
+        })
+        .catch(error => {
+            // console.log(error);
+            this.setState({error: true});
+
         });
     }
 
@@ -31,16 +37,19 @@ class Blog extends Component {
     }
 
     render () {
-        const posts = this.state.posts.map(post => {
-            return (
-                <Post 
-                    key={post.id}
-                    title={post.title}
-                    author={post.author}
-                    clicked={() => this.postSelectedHandler(post.id)}
-                />
-            );
-        });
+        let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>;
+        if (!this.state.error) {
+            posts = this.state.posts.map(post => {
+                return (
+                    <Post 
+                        key={post.id}
+                        title={post.title}
+                        author={post.author}
+                        clicked={() => this.postSelectedHandler(post.id)}
+                    />
+                );
+            });
+        }
 
         return (
             <div>
