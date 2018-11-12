31b09981abaa568471e9b06452681a03059eb405
clinturbin <clinturbin@gmail.com>
2018-11-12 08:18:42 -0500

    React 16 Section 11 Setting Up and Rendering Routes through Using Links to Switch Pages

--- react16-projects/04-reaching-out-to-web/src/containers/Blog/Blog.js
+++ react16-projects/04-reaching-out-to-web/src/containers/Blog/Blog.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
     render () {
@@ -10,12 +12,19 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><a href='/'>Home</a></li>
-                            <li><a href='/new-post'>New Post</a></li>
+                            <li><Link to='/'>Home</Link></li>
+                            <li><Link to={{
+                                pathname: '/new-post',
+                                hash: '#submit', // just an example, doesn't do anything right now
+                                search: '?quick-submit=true'  // just an example, doesn't do anything right now
+                            }}>New Post</Link></li>
                         </ul>
                     </nav>
                 </header>
-                <Posts />
+                {/* <Route path='/' exact render={() => <h1>Home Page</h1>} />
+                <Route path='/' render={() => <h1>Home Page 2</h1>} /> */}
+                <Route path='/' exact component={Posts} />
+                <Route path='/new-post' component={NewPost} />
             </div>
         );
     }
