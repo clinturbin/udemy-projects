1e82f3cec7f6f722fea3de2a0f36f25d13a799a5
clinturbin <clinturbin@gmail.com>
2018-11-15 08:51:42 -0500

    React 16 Section 11 Working with Guards through Loading Routes Lazily

--- react16-projects/04-reaching-out-to-web/src/containers/Blog/Blog.js
+++ react16-projects/04-reaching-out-to-web/src/containers/Blog/Blog.js
@@ -2,10 +2,17 @@ import React, { Component } from 'react';
 import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import './Blog.css';
+import asyncComponent from '../../hoc/asyncComponent';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
+// import NewPost from './NewPost/NewPost';
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost');  // Dynamic Import syntax - only imported when this function is executed
+})
 
 class Blog extends Component {
+    state = {
+        auth: true // should default to false until authenitcated, setting it to true for lazy loading lecture
+    }
     render () {
         return (
             <div className="Blog">
@@ -32,9 +39,13 @@ class Blog extends Component {
                 {/* Switch tells react to only load one of the routes */}
                 {/* The first route that matches a given path will be loaded, it won't render any other route */}
                 <Switch>
-                    <Route path='/new-post' component={NewPost} />
+                    {/* {this.state.auth ? <Route path='/new-post' component={NewPost} /> : null} */}
+                    {this.state.auth ? <Route path='/new-post' component={AsyncNewPost} /> : null}
                     <Route path='/posts' component={Posts} />
-                    <Redirect from="/" to="/posts"/>
+                    <Route render={() => <h1>Not Found</h1>}/>  {/* This route will catch any routes that aren't handled prior */}
+                    {/* <Redirect from="/" to="/posts"/> 
+                        This is also a catch all so it won't work with the above Not Found Route
+                    */}
                 </Switch>
             </div>
         );
