2e84ab4e3b00769afb8501dc4fbe03053edf2360
clinturbin <clinturbin@gmail.com>
2018-11-15 20:23:17 -0500

    React 16 Section 12 Navigating to the Contact Data Component

--- react16-projects/burger-builder/src/containers/Checkout/Checkout.js
+++ react16-projects/burger-builder/src/containers/Checkout/Checkout.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component {
     state = {
@@ -34,6 +36,7 @@ class Checkout extends Component {
                     checkoutCancelled={this.checkoutCancelledHandler}
                     checkoutContinued={this.checkoutContinuedHandler}
                 />
+                <Route path={this.props.match.path + '/contact-data'} component={ContactData} />
             </div>
         );
     }
