cc2b765cb6697fe85dde19c1a182e25402505b9d
clinturbin <clinturbin@gmail.com>
2018-11-17 11:37:55 -0500

    React 16 Section 12 Fetching Orders and Outputting the Orders

--- react16-projects/burger-builder/src/components/Order/Order.js
+++ react16-projects/burger-builder/src/components/Order/Order.js
@@ -2,11 +2,48 @@ import React from 'react';
 
 import classes from './Order.module.css';
 
-const order = (props) => (
-    <div className={classes.Order}>
-        <p>Ingedients: Salad (1)</p>
-        <p>Price: <strong>USD 5.45</strong></p>
-    </div>
-);
+const order = (props) => {
+    // Turn ingredients into an array of ingredients
+    // let transformedIngredients = Object.keys( props.ingredients )
+    //     .map( igKey => {
+    //         return [...Array( props.ingredients[igKey] )].map( ( _, i ) => {
+    //             return <BurgerIngredient key={igKey + i} type={igKey} />;
+    //         } );
+    //     } )
+    //     .reduce((arr, el) => {
+    //         return arr.concat(el)
+    //     }, []);
+
+    //  Alternative
+    const ingredients = [];
+    for (let ingredientName in props.ingredients) {
+        ingredients.push({
+            name: ingredientName,
+            amount: props.ingredients[ingredientName]
+        });
+    }
+
+    const ingredientOutput = ingredients.map(ingredient => {
+        return (
+            <span 
+                style={{
+                    textTransform: 'capitalize',
+                    display: 'inline-block',
+                    margin: '0 8px',
+                    border: '1px solid #ccc',
+                    padding: '5px'
+                }}
+                key={ingredient.name}
+            >{ingredient.name} ({ingredient.amount})</span>
+        );
+    });
+
+    return (
+        <div className={classes.Order}>
+            <p>Ingedients: {ingredientOutput}</p>
+            <p>Price: <strong>USD {Number.parseFloat(props.price).toFixed(2)}</strong></p>
+        </div>
+    );
+};
 
 export default order;
--- react16-projects/burger-builder/src/containers/Orders/Orders.js
+++ react16-projects/burger-builder/src/containers/Orders/Orders.js
@@ -1,16 +1,45 @@
 import React, { Component } from 'react';
 
 import Order from '../../components/Order/Order';
+import axios from '../../axios-orders';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 class Orders extends Component {
+    state = {
+        orders: [],
+        loading: true
+    }
+
+    componentDidMount() {
+        axios.get('/orders.json')
+        .then(response => {
+            console.log(response.data);
+            const fetchedOrders = [];
+            for (let key in response.data) {
+                fetchedOrders.push({
+                    ...response.data[key],
+                    id: key
+                });
+            }
+            this.setState({loading: false, orders: fetchedOrders});
+        }).catch(error => {
+            this.setState({loading: false});
+        })
+    }
+
     render() {
         return (
             <div>
-                <Order />
-                <Order />
+                {this.state.orders.map(order => (
+                    <Order 
+                        key={order.id}
+                        ingredients={order.ingredients}
+                        price={order.price}
+                    />
+                ))}
             </div>
         );
     }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
