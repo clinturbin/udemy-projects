b321ce269665c0653f993ba729ec1e42936b02a6
clinturbin <clinturbin@gmail.com>
2018-11-18 10:54:38 -0500

    React 16 Section 13 Handling User Input and Form Submissions

--- react16-projects/burger-builder/src/containers/Checkout/ContactData/ContactData.js
+++ react16-projects/burger-builder/src/containers/Checkout/ContactData/ContactData.js
@@ -65,10 +65,16 @@ class ContactData extends Component {
 
     orderHandler = (event) => {
         event.preventDefault();
+        const formData = {};
+        for (let formElementIdentifier in this.state.orderForm) {
+            formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
+
+        }
         this.setState({loading: true});
         const order = {
             ingredients: this.props.ingredients,
-            price: this.props.price
+            price: this.props.price,
+            orderData: formData
         }
         axios.post('/orders.json', order)
         .then(response => {
@@ -79,6 +85,18 @@ class ContactData extends Component {
             this.setState({loading: false});
         });
     }
+
+    inputChangedHandler = (event, inputIdentifier) => {
+        const updatedOrderForm = {
+            ...this.state.orderForm
+        };
+        const updatedFormElement = {
+            ...updatedOrderForm[inputIdentifier]
+        };
+        updatedFormElement.value = event.target.value;
+        updatedOrderForm[inputIdentifier] = updatedFormElement;
+        this.setState({orderForm: updatedOrderForm});
+    };
 
     render() {
         const formElementsArray = [];
@@ -91,18 +109,18 @@ class ContactData extends Component {
         }
 
         let form = (
-            <form>
+            <form onSubmit={this.orderHandler}>
                 {formElementsArray.map(formElement => (
                     <Input 
                         key={formElement.id}
                         elementType={formElement.config.elementType}
                         elementConfig={formElement.config.elementConfig}
                         value={formElement.config.value}
+                        changed={(event) => this.inputChangedHandler(event, formElement.id)}
                     />
                 ))}
                 <Button 
-                    btnType='Success'
-                    clicked={this.orderHandler}    
+                    btnType='Success'   
                 >ORDER</Button>
             </form>
         );
