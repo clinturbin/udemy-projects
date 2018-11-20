c6dc53cb193baae2895557caf1edd4527efabbd0
clinturbin <clinturbin@gmail.com>
2018-11-20 10:32:42 -0500

    React 16 Section 14 Assignment 4 Dispatching Actions

--- react16-projects/redux-example/src/containers/Counter/Counter.js
+++ react16-projects/redux-example/src/containers/Counter/Counter.js
@@ -5,35 +5,15 @@ import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
 
 class Counter extends Component {
-    state = {
-        counter: 0
-    }
-
-    counterChangedHandler = ( action, value ) => {
-        switch ( action ) {
-            case 'inc':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + 1 } } )
-                break;
-            case 'dec':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - 1 } } )
-                break;
-            case 'add':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + value } } )
-                break;
-            case 'sub':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - value } } )
-                break;
-        }
-    }
 
     render () {
         return (
             <div>
                 <CounterOutput value={this.props.ctr} />
                 <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
-                <CounterControl label="Decrement" clicked={() => this.counterChangedHandler( 'dec' )}  />
-                <CounterControl label="Add 5" clicked={() => this.counterChangedHandler( 'add', 5 )}  />
-                <CounterControl label="Subtract 5" clicked={() => this.counterChangedHandler( 'sub', 5 )}  />
+                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
+                <CounterControl label="Add 5" clicked={this.props.onAddCounter}  />
+                <CounterControl label="Subtract 5" clicked={this.props.onSubtractCounter}  />
             </div>
         );
     }
@@ -47,7 +27,10 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
     return {
-        onIncrementCounter: () => dispatch({type: 'INCREMENT'})
+        onIncrementCounter: () => dispatch({type: 'INCREMENT'}),
+        onDecrementCounter: () => dispatch({type: 'DECREMENT'}),
+        onAddCounter: () => dispatch({type: 'ADD'}),
+        onSubtractCounter: () => dispatch({type: 'SUBTRACT'}),
     }
 }
 
--- react16-projects/redux-example/src/store/reducer.js
+++ react16-projects/redux-example/src/store/reducer.js
@@ -8,6 +8,22 @@ const reducer = (state = initialState, action) => {
             counter: state.counter + 1
         };
     }
+    if (action.type === 'DECREMENT') {
+        return {
+            counter: state.counter - 1
+        };
+    }
+    if (action.type === 'ADD') {
+        return {
+            counter: state.counter + 5
+        };
+    }
+    if (action.type === 'SUBTRACT') {
+        return {
+            counter: state.counter - 5
+        };
+    }
+
     return state;
 };
 
