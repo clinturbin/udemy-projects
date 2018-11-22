f9eff48c67fa5751326ef18517c0d29357931c85
clinturbin <clinturbin@gmail.com>
2018-11-22 17:20:37 -0500

    React 16 Section 16 Adding Middleware and Using the Redux DevTools

--- react16-projects/redux-example/src/index.js
+++ react16-projects/redux-example/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
@@ -11,9 +11,22 @@ import resultsReducer from './store/reducers/results';
 const rootReducer = combineReducers({
     ctr: counterReducer,
     res: resultsReducer
-})
+});
 
-const store = createStore(rootReducer);
+const logger = store => {
+    return next => {
+        return action => {
+            console.log('[Middleware] Dispatching', action);
+            const result = next(action);
+            console.log('[Middleware] next state', store.getState());
+            return result;
+        };
+    };
+};
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE || compose;
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger)));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
