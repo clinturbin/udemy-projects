352a50d962331f5adcc1fa6c39b47765176d7380
clinturbin <clinturbin@gmail.com>
2018-11-23 11:38:43 -0500

    React 16 Section 16 A Leaner Switch Case Statement

--- react16-projects/redux-example/src/store/reducers/results.js
+++ react16-projects/redux-example/src/store/reducers/results.js
@@ -3,15 +3,17 @@ import { updateObject } from '../utility';
 
 const initialState = {
     results: []
-}
+};
+
+const deleteResult = (state, action) => {
+    const updatedArray = state.results.filter(result => result.id !== action.resultElId )
+    return updateObject(state, { results: updatedArray });
+};
 
 const reducer = (state = initialState, action) => {
     switch(action.type) {
-        case actionTypes.STORE_RESULT:
-            return updateObject(state, { results: state.results.concat({id: new Date(), value: action.result * 2}) });
-        case actionTypes.DELETE_RESULT:
-            const updatedArray = state.results.filter(result => result.id !== action.resultElId )
-            return updateObject(state, { results: updatedArray });
+        case actionTypes.STORE_RESULT: return updateObject(state, { results: state.results.concat({id: new Date(), value: action.result * 2}) });
+        case actionTypes.DELETE_RESULT: return deleteResult(state, action)
     }
     return state;
 };
