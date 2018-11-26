bffaaf21062fe05b533df79a6750bbf131b35025
clinturbin <clinturbin@gmail.com>
2018-11-26 07:10:11 -0500

    React 16 Section 17 Fetching Orders via Redux

--- react16-projects/burger-builder/src/containers/Orders/Orders.js
+++ react16-projects/burger-builder/src/containers/Orders/Orders.js
@@ -1,45 +1,48 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import Order from '../../components/Order/Order';
 import axios from '../../axios-orders';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
+import * as actions from '../../store/actions/index';
+import Spinner from '../../components/UI/Spinner/Spinner';
 
 class Orders extends Component {
-    state = {
-        orders: [],
-        loading: true
-    }
 
     componentDidMount() {
-        axios.get('/orders.json')
-        .then(response => {
-            console.log(response.data);
-            const fetchedOrders = [];
-            for (let key in response.data) {
-                fetchedOrders.push({
-                    ...response.data[key],
-                    id: key
-                });
-            }
-            this.setState({loading: false, orders: fetchedOrders});
-        }).catch(error => {
-            this.setState({loading: false});
-        })
-    }
+        this.props.onFetchOrders();
+    };
 
     render() {
-        return (
-            <div>
-                {this.state.orders.map(order => (
+        let orders = <Spinner />
+        if (!this.props.loading) {
+            orders = this.props.orders.map(order => (
                     <Order 
                         key={order.id}
                         ingredients={order.ingredients}
                         price={order.price}
                     />
-                ))}
+                ));
+        }
+        return (
+            <div>
+                {orders}
             </div>
         );
     }
+};
+
+const mapStateToProps = state => {
+    return {
+        orders: state.order.orders,
+        loading: state.order.loading
+    }
 }
 
-export default withErrorHandler(Orders, axios);
+const mapDispatchToProps = dispatch => {
+    return {
+        onFetchOrders: () => dispatch(actions.fetchOrders())
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(Orders, axios));
--- react16-projects/burger-builder/src/store/actions/order.js
+++ react16-projects/burger-builder/src/store/actions/order.js
@@ -40,3 +40,43 @@ export const purchaseInit = () => {
         type: actionTypes.PURCHASE_INIT
     };
 };
+
+export const fetchOrdersSuccess = (orders) => {
+    return {
+        type: actionTypes.FETCH_ORDERS_SUCCESS,
+        orders: orders
+    };
+};
+
+export const fetchOrdersFail = (error) => {
+    return {
+        type: actionTypes.FETCH_ORDERS_FAIL,
+        error: error
+    };
+};
+
+export const fetchOrdersStart = () => {
+    return {
+        type: actionTypes.FETCH_ORDERS_START
+    };
+};
+
+export const fetchOrders = () => {
+    return dispatch => {
+        dispatch(fetchOrdersStart());
+        axios.get('/orders.json')
+        .then(response => {
+            console.log(response.data);
+            const fetchedOrders = [];
+            for (let key in response.data) {
+                fetchedOrders.push({
+                    ...response.data[key],
+                    id: key
+                });
+            }
+            dispatch(fetchOrdersSuccess(fetchedOrders));
+        }).catch(error => {
+            dispatch(fetchOrdersFail(error));
+        })
+    };
+};
