c48010a9fee46d2fe20e9a0ffa6d84edc798a60b
clinturbin <clinturbin@gmail.com>
2018-12-06 16:31:25 -0500

    React 16 Section 20 How to Test Redux

---
+++ react16-projects/burger-builder/src/store/reducers/auth.test.js
@@ -0,0 +1,33 @@
+import reducer from './auth';
+import * as actionTypes from '../actions/actionTypes';
+
+describe('auth reducer', () => {
+    it('should return the initial state', () => {
+        expect(reducer(undefined, {})).toEqual({
+            token: null,
+            userId: null,
+            error: null,
+            loading: false,
+            authRedirectPath: '/'
+        });
+    });
+    it('should store the token upon login', () => {
+        expect(reducer({
+            token: null,
+            userId: null,
+            error: null,
+            loading: false,
+            authRedirectPath: '/'
+        }, {
+            type: actionTypes.AUTH_SUCCESS,
+            idToken: 'some-token',
+            userId: 'some-userId'
+        })).toEqual({
+            token: 'some-token',
+            userId: 'some-userId',
+            error: null,
+            loading: false,
+            authRedirectPath: '/'
+        });
+    });
+});
