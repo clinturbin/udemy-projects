6110c66e4d81b35a8fa3c56acb30bfc7d868a588
clinturbin <clinturbin@gmail.com>
2018-12-07 19:26:06 -0500

    React 16 Section 23 nextjs A Special Lifecycle Hook

--- react16-projects/nextjs-example/pages/index.js
+++ react16-projects/nextjs-example/pages/index.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Link from 'next/link';
 import Router from 'next/router';
 
-const indexPage = () => (
-    <div>
-        <h1>The Main Page</h1>
-        <p>Go to <Link href="/auth"><a>Auth</a></Link></p>
-        <button onClick={() => Router.push('/auth')}>Go to Auth</button>
-    </div>
-);
+class IndexPage extends Component {
+    static getInitialProps(context) {
+        // console.log(context);
+        const promise = new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve({appName: "Super App"});
+            },1000);
+        });
+        return promise;
+    }
+    render() {
+        return (
+            <div>
+                <h1>The Main Page of {this.props.appName}</h1>
+                <p>
+                    Go to{' '} 
+                    <Link href="/auth">
+                        <a>Auth</a>
+                    </Link>
+                </p>
+                <button onClick={() => Router.push('/auth')}>Go to Auth</button>
+            </div>
+        );
+    }
+};
 
-export default indexPage;
+export default IndexPage;
