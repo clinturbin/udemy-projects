3be850b1537ae6b5aebb02b185aa29a61839ce36
clinturbin <clinturbin@gmail.com>
2018-12-07 19:02:03 -0500

    React 16 Section 23 Understanding the Basics through Handling 404 Errors

--- react16-projects/nextjs-example/pages/auth/index.js
+++ react16-projects/nextjs-example/pages/auth/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
+import User from '../../components/User';
+
 const authIndexPage = () => (
     <div>
         <h1>The Main Auth Page</h1>
+        <User name='C-Bot' age={37} />
     </div>
 );
 
