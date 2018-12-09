dbf02a04f115d198529f8841b453981133208d2d
clinturbin <clinturbin@gmail.com>
2018-12-08 19:14:13 -0500

    React 16 Section 24 Using ReactTransitionGroup

--- react16-projects/react-animations/src/App.js
+++ react16-projects/react-animations/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Transition from 'react-transition-group/Transition';
 
 import "./App.css";
 import Modal from "./components/Modal/Modal";
@@ -7,7 +8,8 @@ import List from "./components/List/List";
 
 class App extends Component {
   state = {
-    modalIsOpen: false
+    modalIsOpen: false,
+    showBlock: false
   }
 
   showModal = () => this.setState({modalIsOpen: true});
@@ -18,8 +20,36 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        <Modal show={this.state.modalIsOpen} closed={this.closeModal}/>
-        <Backdrop show={this.state.modalIsOpen}/>
+        <button 
+          className='Button'
+          onClick={() => this.setState(prevState => ({showBlock: !prevState.showBlock}))}
+        >Toggle</button>
+        <br/>
+        <Transition 
+          in={this.state.showBlock} 
+          timeout={1000}
+          mountOnEnter
+          unmountOnExit
+          >
+          {state => (
+            <div style={{
+              backgroundColor: 'red',
+              width: 100,
+              height: 100,
+              margin: 'auto',
+              transition: 'opacity 1s ease-out',
+              opacity: state === 'exiting' ? 0 : 1
+            }}></div>
+          )}
+        </Transition>
+        {/* {this.state.showBlock ? <div style={{
+          backgroundColor: 'red',
+          width: 100,
+          height: 100,
+          margin: 'auto'
+        }}></div> : null} */}
+        {this.state.modalIsOpen ? <Modal show={this.state.modalIsOpen} closed={this.closeModal}/> : null}
+        {this.state.modalIsOpen ? <Backdrop show={this.state.modalIsOpen} /> : null}
         <button className="Button" onClick={this.showModal}>Open Modal</button>
         <h3>Animating Lists</h3>
         <List />
