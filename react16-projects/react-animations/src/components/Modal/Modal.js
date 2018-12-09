d279a334f17da06de305beafe10ef3a69ac7da5c
clinturbin <clinturbin@gmail.com>
2018-12-09 18:22:02 -0500

    React 16 Section 24 CSSTransition Component and Customizing CSS ClassNames

--- react16-projects/react-animations/src/components/Modal/Modal.js
+++ react16-projects/react-animations/src/components/Modal/Modal.js
@@ -1,32 +1,34 @@
-import React from 'react';
-import Transition from 'react-transition-group/Transition';
+import React from "react";
+import CSSTransition from "react-transition-group/CSSTransition";
 
-import './Modal.css';
+import "./Modal.css";
 
 const animationTiming = {
     enter: 400,
     exit: 1000
 };
 
-const modal = (props) => {
-    return (
-        <Transition mountOnEnter unmountOnExit in={props.show} timeout={animationTiming}>
-          {state => {
-              const cssClasses = [
-                  'Modal',
-                  state === 'entering'
-                  ? 'ModalOpen'
-                  : state === 'exiting' ? 'ModalClosed' : null
-              ];
-              return (
-                <div className={cssClasses.join(' ')}>
-                    <h1>A Modal</h1>
-                    <button className="Button" onClick={props.closed}>Dismiss</button>
-                </div>
-              )
-          }}
-        </Transition>
-    )
+const modal = props => {
+  return (
+    <CSSTransition 
+        mountOnEnter 
+        unmountOnExit 
+        in={props.show} 
+        timeout={animationTiming}
+        classNames={{
+            enter: '',
+            enterActive: 'ModalOpen',
+            exit: '',
+            exitActive: 'ModalClosed'
+        }}>
+          <div className="Modal">
+            <h1>A Modal</h1>
+            <button className="Button" onClick={props.closed}>
+              Dismiss
+            </button>
+          </div>
+    </CSSTransition>
+  );
 };
 
 export default modal;
