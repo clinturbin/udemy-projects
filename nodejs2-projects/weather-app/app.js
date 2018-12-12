234f1a58db8e4a260a3933ba587a60880dafbac1
clinturbin <clinturbin@gmail.com>
2018-12-12 17:22:29 -0500

    Complete Nodejs Section 4 Encoding User Input

--- nodejs2-projects/weather-app/app.js
+++ nodejs2-projects/weather-app/app.js
@@ -1,12 +1,36 @@
 const request = require('request');
+const yargs = require('yargs');
 
 const keys = require('./keys');
 
-console.log(keys.API_KEY);
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+
+// in terminal type node app.js -a 'Enter some address text here'
+
+let encodedAddress = encodeURIComponent(argv.address);
+// encodeURIComponent takes a string like '1301 lombard street' and converts it to 1301%20lombard%20street
+
+console.log(argv.address);
+console.log(encodedAddress);
 
 request({
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia&key=${keys.API_KEY}`,
+    // url: `https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia&key=${keys.API_KEY}`,
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${keys.API_KEY}`,
     json: true
 }, (error, response, body) => {
-    console.log(JSON.stringify(body, undefined, 2));
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 });
