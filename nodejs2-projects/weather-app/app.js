72aafeba5753fdcc6e82135a46e2130c85e3e76a
clinturbin <clinturbin@gmail.com>
2018-12-12 18:20:12 -0500

    Complete Nodejs Section 4 Abstracting Callbacks

--- nodejs2-projects/weather-app/app.js
+++ nodejs2-projects/weather-app/app.js
@@ -1,7 +1,6 @@
-const request = require('request');
 const yargs = require('yargs');
 
-const keys = require('./keys');
+const geocode = require('./geocode/geocode');
 
 const argv = yargs
     .options({
@@ -19,26 +18,10 @@ const argv = yargs
 
 // in terminal type node app.js -a 'Enter some address text here'
 
-let encodedAddress = encodeURIComponent(argv.address);
-// encodeURIComponent takes a string like '1301 lombard street' and converts it to 1301%20lombard%20street
-
-console.log(argv.address);
-console.log(encodedAddress);
-
-request({
-    // url: `https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia&key=${keys.API_KEY}`,
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${keys.API_KEY}`,
-    json: true
-}, (error, response, body) => {
-    if (error) {
-        // System error
-        console.log('Unable to connect to Google servers.');
-    } else if (body.status === 'ZERO_RESULTS') {
-        // Input error
-        console.log('Unable to find that address.');
-    } else if (body.status === "OK") {
-        console.log(`Address: ${body.results[0].formatted_address}`);
-        console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-        console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+    if (errorMessage) {
+        console.log(errorMessage);
+    } else {
+        console.log(JSON.stringify(results, undefined, 2));
     }
 });
