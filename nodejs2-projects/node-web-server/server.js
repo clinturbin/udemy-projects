b6b17236ddcd20528919e74eef09d77d4c03fe60
clinturbin <clinturbin@gmail.com>
2018-12-16 12:12:35 -0500

    Complete Nodejs Section 5 Express Middleware

--- nodejs2-projects/node-web-server/server.js
+++ nodejs2-projects/node-web-server/server.js
@@ -1,10 +1,29 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 let app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    let now = new Date().toString();
+    let log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err) {
+            console.log('Unable to append to server.log.');
+        }
+    });
+    next();
+});
+
+// This basically keeps the site from loading any other routes
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
+
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
