bfc2fcc1904c346baa9e0c497a5591ef107f4634
clinturbin <clinturbin@gmail.com>
2018-12-17 11:25:15 -0500

    Complete Nodejs Section 5 Adding New Feature and Deploying

--- nodejs2-projects/node-web-server/server.js
+++ nodejs2-projects/node-web-server/server.js
@@ -49,6 +49,12 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects'
+    });
+});
+
 app.get('/bad', (req, res) => {
     res.send({
         error: 404,
