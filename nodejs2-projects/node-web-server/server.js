7ac0425ba33bd74b20fb9d783dad7812ae6bd88a
clinturbin <clinturbin@gmail.com>
2018-12-17 10:58:00 -0500

    Complete Node js Section 5 Deploying Your App Setup Start Script and Heroku Port

--- nodejs2-projects/node-web-server/server.js
+++ nodejs2-projects/node-web-server/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
+
 let app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -54,6 +56,6 @@ app.get('/bad', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000');
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
 });
