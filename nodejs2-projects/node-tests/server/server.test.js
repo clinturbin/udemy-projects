2c6a087d9955dfc4102ddad24395f30851f444f5
clinturbin <clinturbin@gmail.com>
2018-12-19 16:57:37 -0500

    Complete Nodejs Section 6 Organizing Test with Describe

--- nodejs2-projects/node-tests/server/server.test.js
+++ nodejs2-projects/node-tests/server/server.test.js
@@ -3,27 +3,33 @@ const expect = require('expect');
 
 const app = require('./server').app;
 
-it('should return Hello World response', (done) => {
-    request(app)
-        .get('/')
-        .expect(404)
-        .expect((res) => {
-            expect(res.body).toInclude({
-                error: 'Page not found'
-            })
-        })
-        .end(done);
-});
-
-it('should return user object', (done) => {
-    request(app)
-        .get('/users')
-        .expect(200)
-        .expect((res) => {
-            expect(res.body).toInclude({
-                name: 'Max',
-                age: 27
-            });
-        })
-        .end(done);
+describe('Server', () => {
+    describe('#root', () => {
+        it('should return Hello World response', (done) => {
+            request(app)
+                .get('/')
+                .expect(404)
+                .expect((res) => {
+                    expect(res.body).toInclude({
+                        error: 'Page not found'
+                    })
+                })
+                .end(done);
+        });
+    });
+    
+    describe('#users', () => {
+        it('should return user object', (done) => {
+            request(app)
+                .get('/users')
+                .expect(200)
+                .expect((res) => {
+                    expect(res.body).toInclude({
+                        name: 'Max',
+                        age: 27
+                    });
+                })
+                .end(done);
+        });
+    });
 });
--- nodejs2-projects/node-tests/utils/utils.test.js
+++ nodejs2-projects/node-tests/utils/utils.test.js
@@ -2,35 +2,34 @@ const expect = require('expect');
 
 const utils = require('./utils');
 
-it('should add 2 numbers', () => {
-    let res = utils.add(33, 11);
-    expect(res).toBe(44).toBeA('number');
-    // if (res !== 44) {
-    //     throw new Error(`Expected 44, but got ${res}.`);
-    // }
+describe('Utils', () => {
+    describe('#add', () => {
+        it('should add 2 numbers', () => {
+            let res = utils.add(33, 11);
+            expect(res).toBe(44).toBeA('number');
+        });
 
-});
-
-it('should async add two numbers', (done) => {
-    utils.asyncAdd(4, 3, (sum) => {
-        expect(sum).toBe(7).toBeA('number');
-        done();
+        it('should async add two numbers', (done) => {
+            utils.asyncAdd(4, 3, (sum) => {
+                expect(sum).toBe(7).toBeA('number');
+                done();
+            });
+        });
     });
-});
 
-it('should async square a number', (done) => {
-    utils.asyncSquare(4, (res) => {
-        expect(res).toBe(16).toBeA('number');
-        done();
+    describe('#square', () => {
+        it('should async square a number', (done) => {
+            utils.asyncSquare(4, (res) => {
+                expect(res).toBe(16).toBeA('number');
+                done();
+            });
+        });
+
+        it('should square 2 numbers', () => {
+            let res = utils.square(3);
+            expect(res).toBe(9).toBeA('number');
+        });
     });
-});
-
-it('should square 2 numbers', () => {
-    let res = utils.square(3);
-    expect(res).toBe(9).toBeA('number');
-    // if (res !== 9) {
-    //     throw new Error(`Expected 9, but got ${res}`);
-    // }
 });
 
 // it('should expect some values', () => {
@@ -58,15 +57,17 @@ it('should square 2 numbers', () => {
 
 // shoudl verify first and last names are set
 // assert it includes firstName and lastName with proper values
-it('should set firstName and lastName', () => {
-    let user = {
-        location: 'Philadelphia',
-        age: 25
-    };
-    let res = utils.setName(user, 'John Doe');
-    expect(res).toInclude({
-        firstName: 'John',
-        lastName: 'Doe'
+describe('Payground', () => {
+    it('should set firstName and lastName', () => {
+        let user = {
+            location: 'Philadelphia',
+            age: 25
+        };
+        let res = utils.setName(user, 'John Doe');
+        expect(res).toInclude({
+            firstName: 'John',
+            lastName: 'Doe'
+        });
     });
 });
 
