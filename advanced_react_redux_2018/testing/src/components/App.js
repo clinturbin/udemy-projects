a3d022fb6c7fe20aa879c8fcb62845662d3f7e1b
clinturbin <clinturbin@gmail.com>
2019-01-31 21:00:03 -0500

    Advanced React and Redux - Section 2 - Added CommentBox and CommentList components

---
+++ advanced_react_redux_2018/testing/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import CommentBox from './CommentBox';
+import CommentList from './CommentList';
+
+export default () => {
+    return (
+        <div>
+            <CommentBox />
+            <CommentList />
+        </div>
+    );
+};
