86069f5177410f6f2603b3ee7105b04dee52b627
clinturbin <clinturbin@gmail.com>
2019-02-01 17:49:13 -0500

    Advanced React and Redux - Section 2 - Added form to CommentBox component

--- advanced_react_redux_2018/testing/src/components/CommentBox.js
+++ advanced_react_redux_2018/testing/src/components/CommentBox.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default () => {
-    return (
-        <div>Comment Box</div>
-    );
+class CommentBox extends Component {
+    state = {
+        comment: ''
+    }
+
+    handleChange = event => {
+        this.setState({comment: event.target.value});
+    };
+
+    handleSubmit = event => {
+        event.preventDefault();
+
+        // TODO - Call an action creator and save the comment
+
+        this.setState({comment: ''});
+    }
+
+    render () {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <h4>Add a comment</h4>
+                <textarea onChange={this.handleChange} value={this.state.comment}/>
+                <div>
+                    <button>Submit Comment</button>
+                </div>
+            </form>
+        )
+    }
 };
+
+export default CommentBox;
