c42e8a37deb79654c7cf00211f913e2472bf605a
clinturbin <clinturbin@gmail.com>
2019-02-01 17:27:26 -0500

    Advanced React and Redux - Section 2 - Refactored app test file using beforeEach

--- advanced_react_redux_2018/testing/src/components/__tests__/App.test.js
+++ advanced_react_redux_2018/testing/src/components/__tests__/App.test.js
@@ -5,12 +5,16 @@ import App from 'components/App';
 import CommentBox from 'components/CommentBox';
 import CommentList from 'components/CommentList';
 
+let wrapped;
+
+beforeEach(() => {
+    wrapped = shallow(<App />);
+});
+
 it('shows a comment box', () => {
-    const wrapped = shallow(<App />);
     expect(wrapped.find(CommentBox).length).toEqual(1);
 });
 
 it('shows a comment list', () => {
-    const wrapped = shallow(<App />);
     expect(wrapped.find(CommentList).length).toEqual(1);
 });
