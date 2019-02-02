4b1500a15567182b0867dad97779864d49b173bf
clinturbin <clinturbin@gmail.com>
2019-02-01 20:05:58 -0500

    Advanced React and Redux - Section 2 Testing - Added test to confirm user can type in textarea box in CommentBox

--- advanced_react_redux_2018/testing/src/components/__tests__/CommentBox.test.js
+++ advanced_react_redux_2018/testing/src/components/__tests__/CommentBox.test.js
@@ -17,3 +17,12 @@ it('has a text area and a button', () => {
     expect(wrapped.find('textarea').length).toEqual(1);
     expect(wrapped.find('button').length).toEqual(1);
 });
+
+it('has a text area that users can type in', () => {
+    wrapped.find('textarea').simulate('change', {
+        target: { value: 'new comment'}
+    });
+    wrapped.update();
+
+    expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+});
