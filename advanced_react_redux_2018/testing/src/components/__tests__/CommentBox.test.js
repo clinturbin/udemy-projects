f86d53d99ad02d58b0e1c8ce9997c2ef1ae942f6
clinturbin <clinturbin@gmail.com>
2019-02-04 14:32:50 -0500

    Advanced React and Redux - Section 2 Testing - Added Root component to fix broken tests

--- advanced_react_redux_2018/testing/src/components/__tests__/CommentBox.test.js
+++ advanced_react_redux_2018/testing/src/components/__tests__/CommentBox.test.js
@@ -2,11 +2,16 @@ import React from 'react';
 import { mount } from 'enzyme';
 // shallow renders an instance of the app component without its children
 import CommentBox from 'components/CommentBox';
+import Root from 'Root';
 
 let wrapped;
 
 beforeEach(() => {
-    wrapped = mount(<CommentBox />);
+    wrapped = mount(
+        <Root>
+            <CommentBox />
+        </Root>
+    );
 });
 
 afterEach(() => {
--- advanced_react_redux_2018/testing/src/index.js
+++ advanced_react_redux_2018/testing/src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import reducers from 'reducers';
+import Root from 'Root';
 
 import App from 'components/App';
 
 ReactDOM.render(
-    <Provider store={createStore(reducers, {})}>
+    <Root>
         <App />
-    </Provider>, 
+    </Root>, 
     document.querySelector('#root')
 );
