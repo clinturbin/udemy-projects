6611654cf079a9f47cadde67bd557d9be032f712
clinturbin <clinturbin@gmail.com>
2019-02-04 13:26:41 -0500

    Advanced React and Redux - Section 2 Testing - Used Describe block to add test to check that textarea gets cleared when form submitted

--- advanced_react_redux_2018/testing/src/components/__tests__/CommentBox.test.js
+++ advanced_react_redux_2018/testing/src/components/__tests__/CommentBox.test.js
@@ -18,11 +18,20 @@ it('has a text area and a button', () => {
     expect(wrapped.find('button').length).toEqual(1);
 });
 
-it('has a text area that users can type in', () => {
-    wrapped.find('textarea').simulate('change', {
-        target: { value: 'new comment'}
+describe('the textarea', () => {
+    beforeEach(() => {
+        wrapped.find('textarea').simulate('change', {
+            target: { value: 'new comment'}
+        });
+        wrapped.update();
+    })
+    it('has a text area that users can type in', () => {
+        expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
     });
-    wrapped.update();
-
-    expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+    
+    it('when form is submitted, textarea gets emptied', () => {
+        wrapped.find('form').simulate('submit');
+        wrapped.update();
+        expect(wrapped.find('textarea').prop('value')).toEqual('');
+    });
 });
