90a1c3d319c331ffde11cede95005ef87f3b90a6
clinturbin <clinturbin@gmail.com>
2019-02-05 12:07:03 -0500

    Advanced React Redux - Section 2 - Added feature to fetch and load comments to page from external api

--- advanced_react_redux_2018/testing/src/components/CommentBox.js
+++ advanced_react_redux_2018/testing/src/components/CommentBox.js
@@ -19,14 +19,17 @@ class CommentBox extends Component {
 
     render () {
         return (
-            <form onSubmit={this.handleSubmit}>
-                <h4>Add a comment</h4>
-                <textarea onChange={this.handleChange} value={this.state.comment}/>
-                <div>
-                    <button>Submit Comment</button>
-                </div>
-            </form>
-        )
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <h4>Add a comment</h4>
+                    <textarea onChange={this.handleChange} value={this.state.comment}/>
+                    <div>
+                        <button>Submit Comment</button>
+                    </div>
+                </form>
+                <button onClick={this.props.fetchComments}>Fetch Comments</button>
+            </div>
+        );
     }
 };
 
