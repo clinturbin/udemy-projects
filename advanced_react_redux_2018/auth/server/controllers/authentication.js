9e39ba6597210c1af328b744f07582afc1919073
clinturbin <clinturbin@gmail.com>
2019-02-13 14:26:52 -0500

    Advanced React Redux - Section 5 Authentication - Updated signup route to search for and create new users

--- advanced_react_redux_2018/auth/server/controllers/authentication.js
+++ advanced_react_redux_2018/auth/server/controllers/authentication.js
@@ -1,3 +1,30 @@
+const User = require('../models/user');
+
 exports.signup = function(req, res, next) {
-    res.send({success: true})
+    const email = req.body.email;
+    const password = req.body.password;
+
+    // See if a user with a given email exists
+    User.findOne({ email: email }, function(err, existingUser) {
+        if (err) { return next(err); }
+
+        // If a user with email does exist, return an error
+        if (existingUser) {
+            return res.status(422).send({ error: 'Email is in use' });
+        }
+
+        // If a user with email does not exist, create and save user record
+        const user = new User({
+            email: email,
+            password: password
+        });
+
+        // saves the user to the database
+        user.save(function(err) {
+            if (err) { return next(err); }
+
+            // Respond to request indicating the user was created
+            res.json({success: true});
+        });
+    });
 };
