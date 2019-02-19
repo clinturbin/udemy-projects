33b5d24d6a5facc8b19e5b9a7904901a66b4658a
clinturbin <clinturbin@gmail.com>
2019-02-18 20:28:15 -0500

    Advanced React Redux - Section 5 Authentication - Finished auth boilerplate by adding signin route

--- advanced_react_redux_2018/auth/server/controllers/authentication.js
+++ advanced_react_redux_2018/auth/server/controllers/authentication.js
@@ -7,6 +7,12 @@ function tokenForUser(user) {
     return jwt.encode({sub: user.id, iat: timeStamp}, config.secret);
 };
 
+exports.signin = function(req, res, next) {
+    // User has already had their email and password authenticated
+    // We just need to give them a token
+    res.send({ token: tokenForUser(req.user) });
+}
+
 exports.signup = function(req, res, next) {
     const email = req.body.email;
     const password = req.body.password;
--- advanced_react_redux_2018/auth/server/services/passport.js
+++ advanced_react_redux_2018/auth/server/services/passport.js
@@ -3,15 +3,37 @@ const User = require('../models/user');
 const config = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+const LocalStrategy = require('passport-local');
+
+// Create Local Strategy
+const localOptions = {
+    usernameField: 'email'
+}
+const localLogin = new LocalStrategy(localOptions, function(email, password, done) {
+    // verify this email and password, call done with the user
+    // if it is the correct email and password
+    // otherwise, call done wiht false
+    User.findOne({ email: email }, function(err, user) {
+        if (err) { return done(err); }
+        if (!user) { return done(null, false); }
+
+        // compare passwords - is 'password' equal to user.password?
+        user.comparePassword(password, function(err, isMatch) {
+            if (err) { return done(err); }
+            if (!isMatch) { return done(null, false) }
+            return done(null, user);
+        });
+    });
+});
 
 // Setup options for JWT Strategy
 const jwtOptions = {
     jwtFromRequest: ExtractJwt.fromHeader('authorization'),
-    secretOrKey = config.secret
+    secretOrKey: config.secret
 };
 
 // Create JWT strategy
-const jwtLogin = new JwtStrategy(jwtOptions( function(payload, done) {
+const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
     // See if the userid in the payload exists in our database
     // If it does, call done with that user 
     // otherwise call done without a user object
@@ -23,7 +45,8 @@ const jwtLogin = new JwtStrategy(jwtOptions( function(payload, done) {
             done(null, false);
         }
     })
-}));
+});
 
 // Tell passport to use this strategy
 passport.use(jwtLogin);
+passport.use(localLogin);
