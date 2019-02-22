94af5d5351d39e429e38bc1e3d5e47c49bb7568d
clinturbin <clinturbin@gmail.com>
2019-02-21 19:51:14 -0500

    Advanced React Redux - Section 6 - Created scaffolding for signup form

--- advanced_react_redux_2018/auth/client/src/index.js
+++ advanced_react_redux_2018/auth/client/src/index.js
@@ -4,11 +4,13 @@ import { BrowserRouter, Route } from 'react-router-dom';
 
 import App from './components/App';
 import Welcome from './components/Welcome';
+import Signup from './components/auth/Signup';
 
 ReactDOM.render(
     <BrowserRouter>
         <App>
             <Route path='/' exact component={Welcome} />    
+            <Route path='/signup' component={Signup} />    
         </App>    
     </BrowserRouter>
 , document.querySelector('#root'));
