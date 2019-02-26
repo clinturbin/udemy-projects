252316d0e0b461c757275743079cdf43291b442a
clinturbin <clinturbin@gmail.com>
2019-02-26 11:07:59 -0500

    Advanced React Redux - Section 6 - Used local storage to persist login state

--- advanced_react_redux_2018/auth/client/src/actions/index.js
+++ advanced_react_redux_2018/auth/client/src/actions/index.js
@@ -5,6 +5,7 @@ export const signup = (formProps, callback) => async dispatch => {
     try {
         const response = await axios.post('http://localhost:3090/signup', formProps);
         dispatch({ type: AUTH_USER, payload: response.data.token });
+        localStorage.setItem('token', response.data.token);
         callback();
     } catch (e) {
         dispatch({ type: AUTH_ERROR, payload: 'Email in use' })
