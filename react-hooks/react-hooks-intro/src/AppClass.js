4c9cee78dab9e3a80e5ed495fd70b3b11f188283
clinturbin <clinturbin@gmail.com>
2019-02-28 19:47:34 -0500

    React Hooks - Use previous state and toggle state state with useState

--- react-hooks/react-hooks-intro/src/AppClass.js
+++ react-hooks/react-hooks-intro/src/AppClass.js
@@ -2,20 +2,40 @@ import React, { Component } from 'react';
 
 class App extends Component {
   state = {
-    count: 0
+    count: 0,
+    isOn: false
   }
 
   incrementCount = () => {
-    this.setState({
-      count: this.state.count + 1
-    })
-  }
+    this.setState(prevState => ({
+      count: prevState.count + 1
+    }));
+  };
+
+  toggleLight = () => {
+    this.setState(prevState => ({
+      isOn: !prevState.isOn
+    }));
+  };
 
   render() {
     return (
-      <button onClick={this.incrementCount}>I was clicked {this.state.count} times(s)</button>
+      <>
+        <h2> Counter</h2>
+        <button onClick={this.incrementCount}>I was clicked {this.state.count} times(s)</button>
+
+        <h2>Toggle Light</h2>
+        <div
+          style={{
+            height: '50px',
+            width: '50px',
+            backgroundColor: this.state.isOn ? 'yellow' : 'grey'
+          }}
+          onClick={this.toggleLight}
+        ></div>
+      </>
     );
-  }
-}
+  };
+};
 
 export default App;
