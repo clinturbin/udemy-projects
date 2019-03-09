b91df24b9f1c369e4238bf7709ac54c547087e79
clinturbin <clinturbin@gmail.com>
2019-03-09 16:34:27 -0500

    React Hooks - CRUD Todo app - created actions to toggle and remove todos

--- react-hooks/hooks-todos/src/components/TodoList.js
+++ react-hooks/hooks-todos/src/components/TodoList.js
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import TodosContext from '../context';
 
 export default function TodoList() {
-    const { state } = useContext(TodosContext);
+    const { state, dispatch } = useContext(TodosContext);
     const title = state.todos.length > 0 ? `${state.todos.length} Todos` : 'Nothing to do!';
 
     return (
@@ -14,7 +14,10 @@ export default function TodoList() {
                         key={todo.id} 
                         className="flex items-center bg-orange-dark border-black border-dashed border-2 my-2 py-4"
                     >
-                        <span className="flex-1 ml-12 cursor-pointer">{todo.text}</span>
+                        <span 
+                            onDoubleClick={() => dispatch({ type: "TOGGLE_TODO", payload: todo })}
+                            className={`flex-1 ml-12 cursor-pointer ${todo.complete && "line-through text-grey-darkest"}`}
+                        >{todo.text}</span>
                         <button>
                             <img
                                 src="https://icon.now.sh/edit/0050c5"
@@ -22,7 +25,9 @@ export default function TodoList() {
                                 className="h-6" 
                             />
                         </button>
-                        <button>
+                        <button
+                            onClick={() => dispatch({ type: "REMOVE_TODO", payload: todo })}
+                        >
                             <img
                                 src="https://icon.now.sh/delete/8b0000"
                                 alt="Delete Icon"
